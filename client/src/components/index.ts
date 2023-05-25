eb3a6a9cdf4af54c6d04a778c2642bf601a0ecb2
yasmws <yasminmwsoares@gmail.com>
2023-05-25 15:51:42 -0300

    feature([Introduction]): -

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,17 +4,20 @@ of it, you can just open the package.json and type ctrl + s and it disappears.
 */
 import { Intro, IntroBackground, IntroTitle, IntroTitleText, IntroText, IntroImg } from "../components/styles";
 
-export const Introduction: React.FC = () => {
-    return(
+import React from 'react';
+
+export const Introduction: React.FC = (props) => {
+    return (
         <Intro>
-                <IntroBackground></IntroBackground>
-                <IntroTitle>QUEM SOMOS?</IntroTitle>
-                <IntroTitleText>Somos a Karuá!</IntroTitleText>
-                <IntroText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </IntroText>
-                <IntroImg></IntroImg>
+            <IntroBackground></IntroBackground>
+            <IntroTitle>QUEM SOMOS?</IntroTitle>
+            <IntroTitleText>Somos a Karuá!</IntroTitleText>
+            <IntroText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </IntroText>
+            <IntroImg></IntroImg>
         </Intro>
-          
     );
 }
+
