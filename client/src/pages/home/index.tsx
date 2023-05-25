9bfc26f20c828edd0e7379b934908407a466730b
yasmws <yasminmwsoares@gmail.com>
2023-05-25 11:42:14 -0300

    feature([Introduction]): small changes

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import { HomeContainer } from './styles';
+import { Intro, IntroBackground, IntroTitle, IntroTitleText, IntroText, IntroImg } from './styles';
 import { LogoCITi } from '../../assets';
 
 
 export const Home: React.FC = () => {
     return (
-        <HomeContainer>
-            <img src={LogoCITi} alt="" />
-            <h1>ReactJS Boilerplate</h1>
-            <p>
-                Made with
-                {' '}
-                <strong>&lt; &#x0002F; &gt;</strong>
-                {' '}
-                and
-                {' '}
-                <strong>&hearts;</strong>
-                {' '}
-                by CITi
-            </p>
-        </HomeContainer>
+        <Intro>
+                <IntroBackground></IntroBackground>
+                <IntroTitle>QUEM SOMOS?</IntroTitle>
+                <IntroTitleText>Somos a Karuá!</IntroTitleText>
+                <IntroText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </IntroText>
+                <IntroImg></IntroImg>
+        </Intro>
     );
 }
