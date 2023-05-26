001bfea2895a894838afe45ed23b57b941a8ad61
yasmws <yasminmwsoares@gmail.com>
2023-05-26 00:37:28 -0300

    feature([Introduction]): -

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -23,26 +23,4 @@ export const Introd: React.ElementType = ({}) => {
     );
 }
 
-/*
-export const Introd: React.ElementType = ({}) => {
-    return (
-        <Intro>
-            <IntroBackground>
-                <video src={VideoKarua} autoPlay loop muted />
-            </IntroBackground>
-            <IntroTitle>QUEM SOMOS?</IntroTitle>
-            <IntroTitleText>Somos a Karuá!</IntroTitleText>
-            <IntroText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </IntroText>
-            <IntroImg>
-                src = {}
-                alt = ''
-            </IntroImg>
-        </Intro>
-    );
-} 
-*/
-
 
