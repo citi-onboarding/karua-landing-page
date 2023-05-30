da493e4fcf9e3a820587c7189575f6521c3dc767
Yasmin Soares <yasminmwsoares@gmail.com>
2023-05-30 09:32:35 -0300

    feature([Introduction]): text above video

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,7 +3,7 @@ a problem with the react, like it has 2 versions
 of it, you can just open the package.json and type ctrl + s and it disappears.
 */
 import Karuau_BRANCA from "../assets/karuau_BRANCA.png";
-import { Intro, Video, IntroBackground, IntroTitle, IntroTitleText, IntroText, IntroImg } from "./styles";
+import { Intro, Video, SameLevel, IntroBackground, IntroTitle, IntroTitleText, IntroText, IntroImg } from "./styles";
 import VideoKarua from './videoKarua.mp4';
 
 export const Introd: React.ElementType = ({}) => {
@@ -14,11 +14,13 @@ export const Introd: React.ElementType = ({}) => {
             </IntroBackground>
             <IntroTitle>QUEM SOMOS?</IntroTitle>
             <IntroTitleText>Somos a Karuá!</IntroTitleText>
-            <IntroText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </IntroText>
-            <IntroImg src={Karuau_BRANCA}/>
+            <SameLevel>
+                <IntroText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </IntroText>
+                <IntroImg src={Karuau_BRANCA}/>
+            </SameLevel>
         </Intro>
     );
 }
--- client/src/components/styles.ts
+++ client/src/components/styles.ts
@@ -1,48 +1,68 @@
-import styled from 'styled-components'; 
+import styled from 'styled-components';
 
 export const Intro = styled.div`
-    
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 export const IntroBackground = styled.div`
-  /* Define your desired styles for the background here */
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
 `;
 
 export const Video = styled.video`
-  /* Define your desired styles for the video here */
-`;
-
-
-export const IntroImg = styled.img`
-
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 export const IntroTitle = styled.div`
-    font-family: 'Xilosa';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 64px;
-    line-height: 30px;
-    /* or 47% */
-
-    text-align: center;
-    font-feature-settings: 'tnum' on, 'lnum' on;
+  font-family: 'Xilosa';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 30px;
+  text-align: center;
+  font-feature-settings: 'tnum' on, 'lnum' on;
+  position: relative;
+  z-index: 1;
+  width: 100%;
 `;
 
 export const IntroTitleText = styled.div`
-    font-family: 'Xilosa';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 64px;
-    line-height: 64px;
+  font-family: 'Xilosa';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 64px;
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  margin-left: 10%; /* Add margin to create spacing */
 `;
 
 export const IntroText = styled.div`
-    font-family: 'Comfortaa';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 18px;
-    text-align: justify;
+  font-family: 'Comfortaa';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 18px;
+  text-align: justify;
+  flex: 1;
+  width: 100%;
+  margin: 0 10% 10%;
 `;
 
+export const SameLevel = styled.div`
+  margin: auto:
+`;
+
+export const IntroImg = styled.img`
+  max-width: 50%;
+  margin: auto;
+`;
