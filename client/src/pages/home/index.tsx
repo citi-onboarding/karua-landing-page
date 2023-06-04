7963c21ec88e5b5ae1d4c027a2ce125e48647c50
DamattaGa <damatta.gabriel@gmail.com>
2023-06-04 17:16:52 -0300

    Add Carousel Mecanism

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { HomeContainer } from './styles';
-import { NavBar } from "../../components";
-import { Footer } from "../../components/Footer";
+import { Footer, Carousel } from "../../components";
+import { NavBar } from "../../components/Navbar";
 
 
 export const Home: React.FC = () => {
     return (
         <HomeContainer>
             <NavBar></NavBar>
+            <Carousel />
             <Footer />
         </HomeContainer>
     );
