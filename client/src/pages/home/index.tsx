9a0d48432f1d5a6933c2fbf2872e54db02565a91
Yasmin Soares <yasminmwsoares@gmail.com>
2023-06-09 14:34:47 -0300

    feature([Introduction]): fixed the video & text

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HomeContainer } from "../Home/styles";
-import { Introd } from "../../components";
+import { Introd } from "../../components/Introduction";
 
 
 export const Home: React.FC = () => {
