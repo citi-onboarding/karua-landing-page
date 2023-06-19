71ec9a7acd3cbfceaeac64f34310f62cd7036f3a
liviabion <llb@cin.ufpe.br>
2023-06-19 12:26:09 -0300

    feature(hotfix/footer): tiktok button

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,13 +2,15 @@ import karuauCOLORIDA from './karuauCOLORIDA_2.svg';
 import LogoCITi from './logo.png';
 import InstaIcon from './instaIcon.svg';
 import FacebookIcon from './facebookIcon.svg';
-import YoutubeIcon from './youtubeIcon.svg'
+import YoutubeIcon from './youtubeIcon.svg';
+import TikTokIcon from './tiktokIcon.svg'
 
 export {
     LogoCITi,
     InstaIcon,
     FacebookIcon,
     YoutubeIcon,
+    TikTokIcon,
     karuauCOLORIDA
 };
 
